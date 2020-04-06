45bcae8102863b67d9b26317ba75733127958f59
huub <huubvh@live.com>
2020-04-06 09:46:16 +0200

    experimented with changing public variables to have them inaccessible outside of the class

--- DiceGame/DiceGame.cs
+++ DiceGame/DiceGame.cs
@@ -6,15 +6,15 @@ namespace DiceGame
     public class DiceGame
 
     {
-        public Enums.resultType result { get; set; }
-        public int bet { get; set; }
-        public int win { get; set; }
-        public int pushDie { get; set; }
-        public PlayerTurn playerTurn { get; set; }
+         public Enums.resultType result { get; set; }
+         int bet { get; set; }
+         int win { get; set; }
+         int pushDie { get; set; }
+         PlayerTurn playerTurn { get; set; }
 
-        public int diceAmount { get; set; }
-        public int diceType { get; set; }
-        public int shoveDie { get; set; }
+         int diceAmount { get; set; }
+         int diceType { get; set; }
+         int shoveDie { get; set; }
 
 
         public void Game(TheHouse.Player player, int diceAmount, int diceType)
@@ -76,6 +76,10 @@ namespace DiceGame
             if (input == "p")
             {
                 Game(player,3,6);
+            }
+            else
+            {
+                Environment.Exit(2);
             }
 
         }
